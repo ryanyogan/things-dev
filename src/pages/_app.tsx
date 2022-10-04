2c7d88a7b145957b8047e23601b2e071c6f18e97
Ryan Yogan <ryan@ryans-mbp-2.lan>
2022-10-04 17:37:06 -0500

    feat: mmm that sweet tRPC is configured

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import "@fontsource/poppins";
+import type { AppProps } from "next/app";
+import { withTRPC } from "@trpc/next";
+import { ServerRouter } from "@/server/router";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default withTRPC<ServerRouter>({
+  config() {
+    const url = process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}`
+      : `http://localhost:3000/api/trpc`;
+
+    return { url };
+  },
+  ssr: true,
+})(App);
